3350c823d82a754bf5a5490e4039e07e2da34bf9
tobias.waller@googlemail.com <tobias.waller@googlemail.com>
2020-10-07 21:51:39 +0000

    CUD

--- src/main/java/tp/kafka/user/KafkaService.java
+++ src/main/java/tp/kafka/user/KafkaService.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ExecutionException;
 import javax.validation.Valid;
 
 import org.apache.kafka.common.protocol.Message;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.messaging.support.MessageBuilder;
@@ -15,18 +16,24 @@ import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Service
-@AllArgsConstructor
 @Log4j2
 public class KafkaService {
+
+    private String userTopic;
     private KafkaTemplate<String, User> kafka;
 
+    public KafkaService(@Value("${user.topics.out.user}") String userTopic, KafkaTemplate<String, User> kafka) {
+        this.kafka = kafka;
+        this.userTopic = userTopic;
+    }
+
     public void saveUser(User user) {
-        kafka.send("input", user.getId().toString(), user)
+        kafka.send(userTopic, user.getId().toString(), user)
             .addCallback(this::logSuccess, this::logFail);
     }
     
     public void deleteUser(UUID id) {
-        kafka.send("input", id.toString(), null)
+        kafka.send(userTopic, id.toString(), null)
             .addCallback(this::logSuccess, this::logFail);
     }
 
